365d0b607f7c78bad763412734907abfb264241a
Ritesh R <72683075+RiteshRavichandran@users.noreply.github.com>
2024-02-24 16:39:14 +0530

    first commit

---
+++ QuoteGen.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pa 
+import requests
+from bs4 import BeautifulSoup
+
+st.title("Quote Generator 🤩")
+st.subheader("", divider="rainbow")
+tag = str(st.selectbox('Choose a topic', ['Love', 'Humor', 'Life', 'Books', 'Friendship', 'Truth', 'Simile'])).lower()
+
+url = f"https://quotes.toscrape.com/tag/{tag}/"
+
+res = requests.get(url)
+
+content = BeautifulSoup(res.content, 'html.parser')
+
+quotes = content.find_all('div', class_='quote')
+
+for quote in quotes:
+    
+    text = quote.find('span', class_='text').text
+    author = quote.find('small', class_='author').text
+    link = quote.find('a')
+    st.success(text)
+    st.markdown(f"<a href=https://quotes.toscrape.com{link['href']}>{author}</a>", unsafe_allow_html=True)
+    st.divider()
+
+st.balloons()
+st.caption('Made with ❤️ from Ritesh')
